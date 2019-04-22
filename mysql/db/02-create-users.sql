11a61a2c771f703873e80e76cb40df0d7a4fd425
KazuyaMiayshita <k_miyashita@itandi.co.jp>
2019-04-22 20:20:34 +0900

    テスト用のサンプルデータを用意。$ docker-compose up の時に用意される。

---
+++ mysql/db/02-create-users.sql
@@ -0,0 +1,22 @@
+use `commenter`;
+
+# auths
+insert into auths (id, email, password) values ("01D90WXJT2CP60TCX74HEK5JEJ", "hoge@example.com", "$2a$10$sBFP36l1Le67XjxZrdlOSeemkbclJu4kiNWtWUK9TEPH.x5o79Jpi"); # みやしー, password
+insert into auths (id, email, password) values ("01D92B2WXJV41YP3G3RVHJSFSV", "fuga@example.com", "$2a$10$nD1pmJ4.pZgomoMrlpTlJurbuWma.KCCOtzWChxl66ECZOiiHnqhG"); # ふにゃ, password
+insert into auths (id, email, password) values ("01D92B4Y5F89PJPPV8Y8BDYY3Y", "piyo@example.com", "$2a$10$IfCQzhOvhMr7jG9phZ0e9.7KORfFDR1MYc3RjAziCFTuncCqZ4Cda"); # ひよこ, password
+
+# users
+insert into users (id, auth_id, name) values ("01D90WXJXCCB10KM24TAPNKHEQ", "01D90WXJT2CP60TCX74HEK5JEJ", "みやしー");
+insert into users (id, auth_id, name) values ("01D92B2WZT4C2PKV70V65W9GJ0", "01D92B2WXJV41YP3G3RVHJSFSV", "ふにゃ");
+insert into users (id, auth_id, name) values ("01D92B4Y7QHGQCK5N7849P6QC4", "01D92B4Y5F89PJPPV8Y8BDYY3Y", "ひよこ");
+
+# tokens
+insert into tokens (token, auth_id, created_at) values ("Bearer 1E5OY/Jck_CMhRicdzTixdgs/JXJtXo4HfbpJmU-IE6gv25Iq/71~lXsX1t0u3xb", "01D92B2WXJV41YP3G3RVHJSFSV", current_timestamp); # みやしー
+insert into tokens (token, auth_id, created_at) values ("Bearer DcCxQARz1Gszsy.OJEuVwtn1MFzwrGS4vuChKOA.RrL+LM6BeKgX4Em5Fy2~30gT", "01D90WXJT2CP60TCX74HEK5JEJ", current_timestamp); # ふにゃ
+insert into tokens (token, auth_id, created_at) values ("Bearer R2hlzuy.IEvUve9Yk~0tj/-SLc8w/JX-w9KwYbPkXqJUFYS4wWP5YQdpcFdtn/ZE", "01D92B4Y5F89PJPPV8Y8BDYY3Y", current_timestamp); # ひよこ
+
+# follows
+insert into follows (follow_user_id, followed_user_id) values ("01D90WXJXCCB10KM24TAPNKHEQ", "01D92B2WZT4C2PKV70V65W9GJ0"); # みやしー -> ふにゃ
+insert into follows (follow_user_id, followed_user_id) values ("01D90WXJXCCB10KM24TAPNKHEQ", "01D92B4Y7QHGQCK5N7849P6QC4"); # みやしー -> ひよこ
+insert into follows (follow_user_id, followed_user_id) values ("01D92B2WZT4C2PKV70V65W9GJ0", "01D92B4Y7QHGQCK5N7849P6QC4"); # ふにゃ -> ひよこ
+insert into follows (follow_user_id, followed_user_id) values ("01D92B4Y7QHGQCK5N7849P6QC4", "01D90WXJXCCB10KM24TAPNKHEQ"); # ひよこ -> みやしー
---
+++ mysql/db/03-create-comments.sql
@@ -0,0 +1,11 @@
+use `commenter`;
+
+# comments
+insert into comments (id, user_id, comment, created_at) values ("01D92C5A6BHQ37SZPC1JNFM7GZ", "01D90WXJXCCB10KM24TAPNKHEQ", "おは", "2019-04-22 11:16:09");
+insert into comments (id, user_id, comment, created_at) values ("01D92C6QYT5D99ZF1MYCM7X35N", "01D90WXJXCCB10KM24TAPNKHEQ", "ねむ", "2019-04-22 11:16:55");
+insert into comments (id, user_id, comment, created_at) values ("01D92C6W6KS0MJV2F4J36QGSWF", "01D90WXJXCCB10KM24TAPNKHEQ", "ピエー", "2019-04-22 11:17:00");
+insert into comments (id, user_id, comment, created_at) values ("01D92C75WZT0KYV66HHTCY6XMH", "01D90WXJXCCB10KM24TAPNKHEQ", "ネコチャン", "2019-04-22 11:17:10");
+insert into comments (id, user_id, comment, created_at) values ("01D92C7C33KKBRD9MAYG1Q7Z55", "01D90WXJXCCB10KM24TAPNKHEQ", "ねむい", "2019-04-22 11:17:16");
+insert into comments (id, user_id, comment, created_at) values ("01D92C7RAK551SFKK5HDNYQX4X", "01D92B2WZT4C2PKV70V65W9GJ0", "ポコー", "2019-04-22 11:17:29");
+insert into comments (id, user_id, comment, created_at) values ("01D92C7W84GGXWZV24PAXBG71W", "01D92B2WZT4C2PKV70V65W9GJ0", "ニャオス", "2019-04-22 11:17:33");
+insert into comments (id, user_id, comment, created_at) values ("01D92C872A03442H3YRGR3BGF6", "01D92B4Y7QHGQCK5N7849P6QC4", "うさぎさん", "2019-04-22 11:17:44");
